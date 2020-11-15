1581a9237f5cf9bb60faef3ea30381f6fef16199
PreyMa <mlpreymann@gmail.com>
2020-11-15 22:56:35 +0100

    Adding support for parenthesis type elements inside root stems
    Calculate top margin and scale values for parenthesis and sum elements inside root stems.

--- static/smr.js
+++ static/smr.js
@@ -15,6 +15,20 @@
     return { child: e, depth };
   }
 
+  function getNthStem( e, depth ) {
+    while( getTagName( e ) === 'm-rtstem' && depth > 0 ) {
+      depth--;
+      e= e.firstElementChild;
+    }
+
+    return e;
+  }
+
+  function parseStyle( elem, name ) {
+    const str= elem.style[name];
+    return parseFloat(str.slice(0, -2));
+  }
+
   function getStyleFontSize( elem ) {
     const style= window.getComputedStyle( elem );
     return parseFloat(style.fontSize.slice(0, -2));
@@ -149,44 +163,91 @@
       const openPars= m.querySelectorAll('m-par[open], m-sum');
       openPars.forEach( e => {
       	const isSum= getTagName( e ) === 'm-sum';
-      	const parent= e.parentNode;
+
+        // Find parent expression if element is wrapped by stem
+        let parent= e, cur= e, rootDepth= -1;
+        while( getTagName(parent) !== 'm-expr' ) {
+          rootDepth++;
+          cur= parent;
+          parent= parent.parentNode;
+          if( !parent ) {
+            console.error('Cannot find parent expression of parenthesis:', e);
+            return;
+          }
+        }
 
         // Get id of closing parenthesis sibling
         const id= e.getAttribute('open');
         const sibling= id ? parent.querySelector(`m-par[close="${id}"]`) : null;
 
         // Check all sibling elements inbetween the paranthesis
-        let cur= e, minTop= Number.POSITIVE_INFINITY, maxBottom= 0;
+        let minTop= Number.POSITIVE_INFINITY, maxBottom= 0;
         while( cur= cur.nextSibling ) {
-        	if( cur === sibling ) {
+
+          // Unwrap elements inside root stems
+          // Sibling might also be wrapped
+          const {child: inner, depth: innerDepth}= getStemChild( cur );
+        	if( inner === sibling ) {
           	break;
           }
 
-          let top= minTop, bottom= maxBottom, d;
-          switch( getTagName( cur ) ) {
-            case 'm-var':
-            case 'm-op':
-            case 'm-nm':
-            case 'm-sub': // ?
-            case 'm-sup':
-            case 'm-lbnd':
-            case 'm-ubnd':
-            case 'm-num':
-            case 'm-denom': // ?
-              d= cur.firstElementChild;
-              top= d.offsetTop;
-              bottom= d.offsetTop+ d.clientHeight;
-              break;
+          let top= minTop, bottom= maxBottom;
+          function calcTopBottom( cur ) {
+            let d;
+            switch( getTagName( cur ) ) {
+              case 'm-var':
+              case 'm-op':
+              case 'm-nm':
+              case 'm-sub': // ?
+              case 'm-sup':
+              case 'm-lbnd':
+              case 'm-ubnd':
+              case 'm-num':
+              case 'm-denom': // ?
+                d= cur.firstElementChild;
+                top= d.offsetTop;
+                bottom= d.offsetTop+ d.clientHeight;
+                break;
 
-            case 'm-par':
-            case 'm-sum':
-              top= cur.offsetTop;
-              bottom= cur.offsetTop+ cur.clientHeight;
-              break;
+              case 'm-par':
+              case 'm-sum':
+                top= cur.offsetTop;
+                bottom= cur.offsetTop+ cur.clientHeight;
+                break;
 
-            default:
-              break;
+              case 'm-spacer':
+                // Only consider top spacers
+                if( cur.getAttribute('r') === '1' ) {
+                  // Set top offset if the root level is equal or greater
+                  d= getNthStem( cur.firstElementChild, rootDepth );
+                  if( d ) {
+                    top= d.offsetTop;
+                  }
+                }
+                break;
+
+
+              case 'm-rtstem':
+                // Get element at root depth and
+                d= getNthStem( cur, rootDepth );
+                if( d !== cur ) {
+                  // Don't recur if another stem element is found
+                  if( getTagName(d) === 'm-rtstem' ) {
+                    top= d.offsetTop;
+
+                  } else {
+                    calcTopBottom( d );
+                  }
+                }
+                break;
+
+              default:
+                break;
+            }
           }
+
+          // Evaluate element
+          calcTopBottom( cur );
 
           // Find highest and lowest points
           maxBottom= Math.max(maxBottom, bottom);
@@ -195,12 +256,15 @@
 
         // Reset minTop if the loop did not run at least once
         if( !e.nextSibling ) {
-          minTop= 0;
+          minTop= e.offsetTop;
         }
 
         // Get font size of the parent expression
         const fontSize= getStyleFontSize( parent );
         const size= maxBottom- minTop;
+
+        // There might be a root stem that creates a top offset
+        const marginOffset= minTop- e.offsetTop;
 
         function s(q) {
           return q ? q.firstElementChild.style : {};
@@ -210,13 +274,13 @@
         if( isSum ) {
         	const fs= Math.floor( size* 0.7 );
         	s(e).fontSize= `${fs}px`;
-          s(e).marginTop= `${minTop}px`;
+          s(e).marginTop= `${marginOffset}px`;
 
         } else {
           const scaleY= size/fontSize;
           const scaleX= Math.min(1.1, Math.max(0.7, scaleY / 1.3));
           s(e).transform= s(sibling).transform= `scale(${scaleX}, ${scaleY})`;
-          s(e).marginTop= s(sibling).marginTop= `${minTop}px`;
+          s(e).marginTop= s(sibling).marginTop= `${marginOffset}px`;
         }
       });
     });
